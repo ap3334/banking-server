ce9fd717cd8c93cbc8a89e50fc1ed642452b55a6
ap3334 <chwil2989@naver.com>
2023-02-16 21:26:43 +0900

    test : UserController 회원가입 API 테스트 코드 생성 #3

---
+++ src/test/java/com/example/bankingserver/web/UserApiControllerTest.java
@@ -0,0 +1,76 @@
+package com.example.bankingserver.web;
+
+import com.example.bankingserver.domain.UserRepository;
+import com.example.bankingserver.domain.Users;
+import com.example.bankingserver.web.dto.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void 회원가입_ApiTest() throws Exception {
+
+        // given
+        UserDto userDto = UserDto.builder()
+                .username("test")
+                .password("1234")
+                .build();
+
+        String url = "http://localhost:" + port + "/users";
+
+        // when
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(userDto)))
+                .andExpect(status().isOk());
+
+        // then
+        List<Users> all = userRepository.findAll();
+        assertThat(all.get(0).getUsername()).isEqualTo(userDto.getUsername());
+
+    }
+
+
+}
