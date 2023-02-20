2173c1dda33064eb4c0bb5bae6f67acd1742ab24
ap3334 <chwil2989@naver.com>
2023-02-20 09:06:15 +0900

    test : 친구 추가, 목록 조회 test 코드 작성

--- src/test/java/com/example/bankingserver/web/UserApiControllerTest.java
+++ src/test/java/com/example/bankingserver/web/UserApiControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.bankingserver.web;
 
+import com.example.bankingserver.domain.Friendship;
 import com.example.bankingserver.domain.UserRepository;
 import com.example.bankingserver.domain.Users;
 import com.example.bankingserver.web.dto.UserDto;
+import com.example.bankingserver.web.dto.UsernameDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
@@ -20,8 +23,9 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserApiControllerTest {
 
@@ -58,7 +62,7 @@ class UserApiControllerTest {
                 .password("1234")
                 .build();
 
-        String url = "http://localhost:" + port + "/users";
+        String url = "http://localhost:" + port + "/user";
 
         // when
         mvc.perform(post(url)
@@ -72,5 +76,106 @@ class UserApiControllerTest {
 
     }
 
+    @Test
+    public void 친구추가_ApiTest_성공() throws Exception {
 
+        // given
+        Users user1 = userRepository.save(new Users("test1", "1234"));
+        Users user2 = userRepository.save(new Users("test2", "1234"));
+        Users user3 = userRepository.save(new Users("test3", "1234"));
+
+        UsernameDto usernameDto2 = new UsernameDto(user2.getUsername());
+        UsernameDto usernameDto3 = new UsernameDto(user3.getUsername());
+
+        String url = "http://localhost:" + port + "/user/" + user1.getId() + "/friend";
+
+        // when
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(usernameDto2)))
+                .andExpect(status().isOk());
+
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(usernameDto3)))
+                .andExpect(status().isOk());
+
+        // then
+        List<Friendship> friendshipList = userRepository.findById(user1.getId()).get().getFriendshipList();
+        assertThat(friendshipList.size()).isEqualTo(2);
+        assertThat(friendshipList.get(0).getFriend().getUsername()).isEqualTo("test2");
+
+    }
+
+    @Test
+    public void 친구추가_ApiTest_실패_사용자가본인을친구추가할때() throws Exception {
+
+        // given
+        Users user1 = userRepository.save(new Users("test1", "1234"));
+
+        UsernameDto usernameDto1 = new UsernameDto("test1");
+
+        String url = "http://localhost:" + port + "/user/" + user1.getId() + "/friend";
+
+        // when
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(usernameDto1)))
+                .andExpect(status().isForbidden());
+
+        // then
+        List<Friendship> friendshipList = userRepository.findById(user1.getId()).get().getFriendshipList();
+        assertThat(friendshipList.size()).isEqualTo(0);
+
+    }
+
+    @Test
+    public void 친구추가_ApiTest_실패_존재하지않는사용자친구추가할때() throws Exception {
+
+        // given
+        Users user = userRepository.save(new Users("test1", "1234"));
+
+        UsernameDto usernameDto = new UsernameDto("test2");
+
+        String url = "http://localhost:" + port + "/user/" + user.getId() + "/friend";
+
+        // when
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(usernameDto)))
+                .andExpect(status().is4xxClientError());
+
+        // then
+        List<Friendship> friendshipList = userRepository.findById(user.getId()).get().getFriendshipList();
+        assertThat(friendshipList.size()).isEqualTo(0);
+
+    }
+
+    @Test
+    public void 친구추가_ApiTest_실패_중복친구추가할때() throws Exception {
+
+        // given
+        Users user1 = userRepository.save(new Users("test1", "1234"));
+        Users user2 = userRepository.save(new Users("test2", "1234"));
+
+        UsernameDto usernameDto = new UsernameDto(user2.getUsername());
+
+        String url = "http://localhost:" + port + "/user/" + user1.getId() + "/friend";
+
+        // when
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(usernameDto)))
+                .andExpect(status().isOk());
+
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(usernameDto)))
+                .andExpect(status().isBadRequest());
+
+        // then
+        List<Friendship> friendshipList = userRepository.findById(user1.getId()).get().getFriendshipList();
+        assertThat(friendshipList.size()).isEqualTo(1);
+
+    }
 }
