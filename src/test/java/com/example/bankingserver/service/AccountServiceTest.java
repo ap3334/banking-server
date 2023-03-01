4d23feff74677c098e2f2e659abccf3a7e728740
ap3334 <chwil2989@naver.com>
2023-03-02 07:24:37 +0900

    test : 계좌이체 테스트코드 추가

--- src/test/java/com/example/bankingserver/service/AccountServiceTest.java
+++ src/test/java/com/example/bankingserver/service/AccountServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.bankingserver.service;
 
 import com.example.bankingserver.domain.*;
+import com.example.bankingserver.exception.AccountNotFoundException;
+import com.example.bankingserver.exception.BadRequestException;
+import com.example.bankingserver.exception.ForbiddenException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +14,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class AccountServiceTest {
@@ -26,6 +30,83 @@ class AccountServiceTest {
 
     @Autowired
     private AccountService accountService;
+
+    @Test
+    public void 계좌이체_성공_싱글쓰레드() throws Exception {
+
+        // given
+        Users userA = new Users("userA", "1234");
+        Users userB = new Users("userB", "1234");
+        userRepository.saveAll(List.of(userA, userB));
+
+        Friendship friendship = new Friendship(userA, userB);
+        friendshipRepository.save(friendship);
+
+        Account accountA = new Account(userA, 20000);
+        Account accountB = new Account(userB, 2000);
+        accountRepository.saveAll(List.of(accountA, accountB));
+
+        accountService.transferAccount(accountA.getId(), accountB.getId(), 1000);
+
+        // then
+        Account resultA = accountRepository.findByUserId(userA.getId()).get();
+        Account resultB = accountRepository.findByUserId(userB.getId()).get();
+
+        assertThat(resultA.getBalance()).isEqualTo(19000);
+        assertThat(resultB.getBalance()).isEqualTo(3000);
+
+    }
+
+    @Test
+    public void 계좌이체_실패_계좌존재하지않을경우() throws Exception {
+
+        // given
+        Users userA = new Users("userA", "1234");
+        Users userB = new Users("userB", "1234");
+        userRepository.saveAll(List.of(userA, userB));
+
+        Friendship friendship = new Friendship(userA, userB);
+        friendshipRepository.save(friendship);
+
+        // when
+        assertThrows(AccountNotFoundException.class, () -> accountService.transferAccount(1L, 2L, 1000));
+
+    }
+
+    @Test
+    public void 계좌이체_실패_친구가아닌경우() throws Exception {
+
+        // given
+        Users userA = new Users("userA", "1234");
+        Users userB = new Users("userB", "1234");
+        userRepository.saveAll(List.of(userA, userB));
+
+        Account accountA = new Account(userA, 20000);
+        Account accountB = new Account(userB, 2000);
+        accountRepository.saveAll(List.of(accountA, accountB));
+
+        assertThrows(ForbiddenException.class, () -> accountService.transferAccount(1L, 2L, 1000));
+
+    }
+
+    @Test
+    public void 계좌이체_실패_잔액부족한경우() throws Exception {
+
+        // given
+        Users userA = new Users("userA", "1234");
+        Users userB = new Users("userB", "1234");
+        userRepository.saveAll(List.of(userA, userB));
+
+        Friendship friendship = new Friendship(userA, userB);
+        friendshipRepository.save(friendship);
+
+        Account accountA = new Account(userA, 500);
+        Account accountB = new Account(userB, 2000);
+        accountRepository.saveAll(List.of(accountA, accountB));
+
+        assertThrows(BadRequestException.class, () -> accountService.transferAccount(1L, 2L, 1000));
+
+    }
 
     @Test
     public void 계좌이체_멀티쓰레드_동시성테스트() throws Exception {
