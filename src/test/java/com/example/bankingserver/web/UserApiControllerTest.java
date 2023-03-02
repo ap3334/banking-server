74012151ab8428603bc0fecb64e6d58759523ea1
ap3334 <chwil2989@naver.com>
2023-03-02 20:20:31 +0900

    test : 중복 Username으로 가입 시 예외 발생 test

--- src/test/java/com/example/bankingserver/web/UserApiControllerTest.java
+++ src/test/java/com/example/bankingserver/web/UserApiControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
