674b9389325acc2b03e358515ecc5605dcf33cd7
ap3334 <chwil2989@naver.com>
2023-03-20 21:48:25 +0900

    refactor: SecurityConfig 파일 수정

--- src/main/java/com/example/bankingserver/config/auth/SecurityConfig.java
+++ src/main/java/com/example/bankingserver/config/auth/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.bankingserver.config.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -19,11 +21,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll();
-
+                .requestMatchers("/auth/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable();
 
         return http.build();
     }
